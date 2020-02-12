b005f12f702f8d81e4fd9744a73fa7962b0064b8
miriam-sullivan <maljayd42@gmail.com>
2020-02-12 16:51:05 -0600

    just need to implement the hoo into the actual component

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
-import Navbar from "../components/Navbar";
-function useDarkmode() {
+
+export default function useDarkmode() {
   const [DarkModeOnOrOff, setDarkModeOnOrOff] = useLocalStorage(
     "DarkModeToggle"
   );
@@ -9,5 +9,6 @@ function useDarkmode() {
     useLocalStorage(DarkModeOnOrOff)
       ? document.body.classList.add("dark-mode")
       : document.body.classList.remove("dark-mode");
-  }, [Navbar]);
+  }, [DarkModeOnOrOff]);
+  return [DarkModeOnOrOff, setDarkModeOnOrOff];
 }
